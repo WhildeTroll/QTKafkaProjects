9c2cbca5eb93dd261d4be8e22cbb28fe77fe8971
WhildeTroll <danila_selyaev@mail.ru>
2025-12-17 11:30:52 +0400

    startproj

--- Publisher/main.cpp
+++ Publisher/main.cpp
@@ -6,8 +6,38 @@
 
 int main(int argc, char *argv[])
 {
-    char hostname = 'localhost:9092';
     char errstr[512];
-    rd_kafka_conf_t *conf = rd_kafka_conf_new();
-    rd_kafka_produce();
+    rd_kafka_t *rk;
+    rd_kafka_conf_t *conf;
+    rd_kafka_topic_conf_t *topic_conf;
+    rd_kafka_topic_t *rkt;
+    const char *hostname = "localhost:9092";
+    const char *topic = "test-topic";
+    const char *massage = "Hello Kafka";
+
+    rd_kafka_conf_set(conf, "bootstrap.servers", hostname, errstr, sizeof (errstr) != RD_KAFKA_CONF_OK){
+        fprintf(stderr, "Ошибка настройки брокеров %s\n", errstr);
+        return 1;
+    }
+
+    topic_conf = rd_kafka_conf_new();
+
+    rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof (errstr));
+    if (!rk) {
+        fprintf(stderr, "Ошибка создания продюсера %s\n", errstr);
+        return 1;
+    }
+
+    rkt = rd_kafka_topic_new(rk, topic, topic_conf);
+    if (!rkt){
+        fprintf(stderr, "Ошибка создания топика %s\n", errstr);
+        rd_kafka_destroy(rk);
+    }
+    rd_kafka_produce(rkt, RD_KAFKA_PARTITION_UA,
+                  RD_KAFKA_MSG_F_COPY,
+                  (void *)massage, strlen(massage),
+                  NULL, 0,
+                  NULL);
+
+
 }
